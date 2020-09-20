ac0221c4de5154a0d675f692fc335f7ac6133b19
Aremu Oluwaseyi Festus <Rem_files@Aremus-MacBook-Air.local>
2020-09-20 09:54:09 +0200

    Added serializers to api app

---
+++ libStash/api/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from books.models import Publisher, Book, Author, Warehouse, WarehouseBook, Image
+
+
+class PublisherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publisher
+        fields = ["name", "address", "phone", "url"]
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ["name", "phone", "address"]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = [
+            "title",
+            "author",
+            "publisher",
+            "category",
+            "format",
+            "isbn",
+            "year",
+            "price",
+            "last_update",
+        ]
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ["book", "book_cover", "last_update"]
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warehouse
+        fields = ["address", "phone"]
+
+
+class WarehouseBookSerilizer(serializers.ModelSerializer):
+    class meta:
+        model = WarehouseBook
+        fields = ["warehouse", "book", "count"]
