907464fdbaa2b1c922ac3046d8b65437cea4e72a
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2021-03-26 01:35:54 +0100

    Set up gitlab configurations

--- libStash/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-python3.8 manage.py collectstatic --noinput --settings=libStash.settings.production 
-python3.8 manage.py makemigrations --settings=libStash.settings.production 
-python3.8 manage.py migrate --settings=libStash.settings.production
-
-gunicorn libStash.wsgi:application --bind 0.0.0.0:$PORT
